f37f75fa919d2c4e24323bdcfd10a94a54f394d5
Saurabh Mahajan <saurabh_mahajan29@yahoo.co.in>
2025-04-03 12:23:19 +0530

    first episode of namaste react

---
+++ App.js
@@ -0,0 +1,10 @@
+// React code
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from React !!!"
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
